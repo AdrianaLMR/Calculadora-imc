0d7daa62d9680c41f2b7d0b432a28c5ac76ba362
AdrianaLMR <trevenlynk038@gmail.com>
2023-02-02 13:19:14 -0300

    commit atualizando código

--- calculadora imc/assets/js/script.js
+++ calculadora imc/assets/js/script.js
@@ -13,20 +13,20 @@ function imc () {
         let classificacao = '';
 
         if (valorIMC < 18.5){
-            classificacao = 'abaixo do peso.';
+            classificacao = 'Abaixo do peso.';
         }else if (valorIMC < 25) {
-            classificacao = 'com peso ideal. Parabéns!!!';
+            classificacao = 'Peso ideal. Parabéns!!!';
         }else if (valorIMC < 30){
-            classificacao = 'levemente acima do peso.';
+            classificacao = 'Levemente acima do peso.';
         }else if (valorIMC < 35){
-            classificacao = 'com obesidade grau I.';
+            classificacao = 'Obesidade grau I.';
         }else if (valorIMC < 40){
-            classificacao = 'com obesidade grau II';
+            classificacao = 'Obesidade grau II';
         }else {
-            classificacao = 'com obesidade grau III. Cuidado!!';
+            classificacao = 'Obesidade grau III. Cuidado!!';
         }
 
-        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+        resultado.textContent = `${nome} seu IMC é ${valorIMC}, sua classificação é: ${classificacao}`;
         
     }else {
         resultado.textContent = 'Preencha todos os campos!!!';
